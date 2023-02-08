a1fbaf848e05ff736b3747d062254e15c592afc5
I Made Aris Aditya Nugraha <arisaditya301@gmail.com>
2023-02-09 01:02:14 +0800

    delete seeder data

--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -12,24 +12,6 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //
-        // $students = [
-        //     [
-        //         'major_id'=> 1,
-        //         'nim'=> '1905551088',
-        //         'nama'=> 'Aris Aditya',
-        //         'semester'=> 8,
-        //         'tanggal_masuk' =>'2023-02-02'
-        //     ],
-        //     [
-        //         'major_id'=> 3,
-        //         'nim'=> '1905551095',
-        //         'nama'=> 'Smara Putra',
-        //         'semester'=> 8,
-        //         'tanggal_masuk' =>'2023-02-02'
-        //     ]
-        //     ];
-
-        //     Student::insert($students);
+        
     }
 }
